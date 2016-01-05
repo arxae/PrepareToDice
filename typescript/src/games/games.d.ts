e6aeb0381844c5e5b0b5f81e95f627c7e6fa331f
Arxae <maesenandy@gmail.com>
2016-01-05 19:36:01 +0100

    Bah

--- typescript/src/games/games.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="darksouls1.ts" />
-/// <reference path="darksouls2.ts" />
-/// <reference path="bloodborne.ts" />
--- typescript/typings/defs.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="chance.d.ts" />
-/// <reference path="jquery.d.ts" />
-/// <reference path="sweetalert.d.ts" />
-/// <reference path="storejs.d.ts" />
